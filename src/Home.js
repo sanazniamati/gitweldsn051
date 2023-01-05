5319fc6fdd80f8c0c80590f82d2f803d0e9f012b
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-01-05 20:24:11 +0330

    11

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import HY from "../src/assets/hy.png";
+import steep_flanked from "../src/assets/steep_flanked.png";
 import { Link } from "react-router-dom";
 function Home() {
   return (
@@ -7,7 +8,7 @@ function Home() {
         <img width="570px" src={HY} alt="profile" />
       </Link>
       <Link to="/SteepFlankedSingleV">
-        <img width="570px" src={HY} alt="profile" />
+        <img width="570px" src={steep_flanked} alt="profile" />
       </Link>
     </>
   );
